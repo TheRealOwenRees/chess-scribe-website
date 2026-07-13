7994f79e02e682fd3d564c28a92757ffa1fcdec5
Owen <owenrees.dev@gmail.com>
2026-07-13 20:31:33 +0200

    remove build envs, change zod items to url

--- src/env.ts
+++ src/env.ts
@@ -1,16 +1,16 @@
 import { z } from "zod";
 
 const clientSchema = z.object({
-	VITE_API_BASE_URL: z.string("Invalid API Base URL format"),
-	VITE_MATOMO_URL: z.string("Invalid URL format"),
+	VITE_API_BASE_URL: z.url("Invalid API Base URL format"),
+	VITE_MATOMO_URL: z.url("Invalid URL format"),
 	VITE_MATOMO_SITE_ID: z.string("Invalid Site ID"),
 	VITE_MAINTENANCE_MODE: z.string("Invalid Maintenance Mode"),
 	VITE_MAINTENANCE_MODE_MESSAGE: z.string().optional(),
 });
 
 const serverSchema = z.object({
-	DISCORD_CONTACT_WEBHOOK: z.string("Invalid Discord Contact Webhook URL"),
-	DISCORD_ERROR_LOG_WEBHOOK: z.string("Invalid Discord Error Log Webhook URL"),
+	DISCORD_CONTACT_WEBHOOK: z.url("Invalid Discord Contact Webhook URL"),
+	DISCORD_ERROR_LOG_WEBHOOK: z.url("Invalid Discord Error Log Webhook URL"),
 	LICHESS_CLIENT_ID: z.string("Invalid Lichess Client ID"),
 });
 

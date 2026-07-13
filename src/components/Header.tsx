908dd9b57b431cf576e5d75477448ad67e805fc2
Owen <owenrees.dev@gmail.com>
2026-07-13 21:00:26 +0200

    parse maintenance mode message

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,26 @@
 import { Link } from "@tanstack/react-router";
 import { Image } from "@unpic/react";
+import { z } from "zod";
 import MaintenanceModeBanner from "#/components/MaintenanceModeBanner.tsx";
 import { env } from "#/env.ts";
 import logo from "@/assets/images/logo.svg?url";
 
 export default function Header() {
+	const maintenanceModeMessage = env.VITE_MAINTENANCE_MODE
+		? z
+				.string()
+				.transform((value) => (value.trim() === "" ? null : value))
+				.parse(env.VITE_MAINTENANCE_MODE_MESSAGE)
+		: null;
+
 	return (
 		<header>
-			{env.VITE_MAINTENANCE_MODE ? (
+			{maintenanceModeMessage ? (
 				<div className="absolute w-full">
-					<MaintenanceModeBanner message={env.VITE_MAINTENANCE_MODE_MESSAGE} />
+					<MaintenanceModeBanner message={maintenanceModeMessage} />
 				</div>
 			) : null}
+
 			<nav className="w-full max-w-screen-2xl place-self-center p-8">
 				<div className="container mx-auto flex items-center justify-between">
 					<div className="flex items-center">
